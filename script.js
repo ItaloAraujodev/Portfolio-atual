d68f56a164878278e23672b33ca63df8ae6693b3
ItaloAraujodev <italoarauju2019@gmail.com>
2024-01-06 14:35:15 -0300

    Portfolio

---
+++ script.js
@@ -0,0 +1,17 @@
+const sr = ScrollReveal ({
+  distance: "35px",
+  duration: 2400,
+  reset: true,
+});
+
+sr.reveal('.main-img', { delay: 210, origin: 'left' });
+sr.reveal('.main-text h1', { delay: 210, origin: 'top' });
+sr.reveal('.main-text h4', { delay: 210, origin: 'right' });
+sr.reveal('.social', { delay: 210, origin: 'bottom' });
+
+
+sr.reveal('.row', { delay: 310, origin: 'left' });
+sr.reveal('.button', { delay: 410, origin: 'top' });
+sr.reveal('.center', { delay: 310, origin: 'right' });
+
+sr.reveal('.portfolio', { delay: 410, origin: 'top' });
